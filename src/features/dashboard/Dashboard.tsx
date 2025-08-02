8b2668a9315ce3584a2a9aa44fd1896a0ac2d0ba
DVitaliy <dynnik.vitaliy@gmail.com>
2025-08-02 19:30:02 +0300

    added shadcn

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/shared/ui/kit/button";
+
 function Dashboard() {
   return (
     <div className="text-3xl text-red-500 underline">
       Welcome to the Dashboard
+      <Button>Click Me</Button>
     </div>
   );
 }
